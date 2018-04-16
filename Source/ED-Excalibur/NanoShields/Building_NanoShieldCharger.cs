25fa0c27edaed377154ac0b29c541d1952211e0c
Joshua W <jaxxa90@gmail.com>
2018-04-16 17:49:53 +1000

    Initial Version of NanoShield

---
+++ Source/ED-Excalibur/NanoShields/Building_NanoShieldCharger.cs
@@ -0,0 +1,220 @@
+﻿using EnhancedDevelopment.Excalibur.Settings;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace EnhancedDevelopment.Excalibur.NanoShields
+{
+    [StaticConstructorOnStartup]
+    class Building_NanoShieldCharger : Building
+    {
+        #region Variables
+
+        private int ReservePowerCurrent = 0;
+
+        public float MAX_DISTANCE = 5.0f;
+        public bool flag_charge = false;
+        CompPowerTrader power;
+
+        private static Texture2D UI_UPGRADE;
+        private static Texture2D UI_CHARGE_OFF;
+        private static Texture2D UI_CHARGE_ON;
+
+        #endregion
+
+        //Constructor
+        static Building_NanoShieldCharger()
+        {
+            //Log.Message("Getting graphics");
+            UI_UPGRADE = ContentFinder<Texture2D>.Get("UI/NanoShield/Upgrade", true);
+            UI_CHARGE_OFF = ContentFinder<Texture2D>.Get("UI/NanoShield/ChargeOFF", true);
+            UI_CHARGE_ON = ContentFinder<Texture2D>.Get("UI/NanoShield/ChargeON", true);
+        }
+
+        #region Overrides
+
+        //Dummy override
+        public override void PostMake()
+        {
+            base.PostMake();
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+        }
+
+        //On spawn, get the power component reference
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            this.power = base.GetComp<CompPowerTrader>();
+        }
+
+        public override void Tick()
+        {
+            //Log.Message("Tick");
+            base.Tick();
+
+
+            int currentTick = Find.TickManager.TicksGame;
+            //Only every 10 ticks
+            if (currentTick % Mod_EDExcalibur.Settings.BuildingChargeDelay != 0)
+            {
+                return;
+            }
+            if (this.power.PowerOn == true)
+            {
+                if (this.flag_charge)
+                {
+                    this.rechargePawns(Mod_EDExcalibur.Settings.BuildingChargeAmount);
+                }
+
+                if (this.ReservePowerCurrent < Mod_EDExcalibur.Settings.BuildingReservePowerMax)
+                {
+                    this.ReservePowerCurrent += 1;
+                }
+            }
+            else
+            {
+
+                if (this.flag_charge)
+                {
+                    if (this.ReservePowerCurrent > 0)
+                    {
+                        this.ReservePowerCurrent -= 1;
+                        this.ReservePowerCurrent += this.rechargePawns(1);
+                    }
+                }
+            }
+        }
+
+        public override void DrawExtraSelectionOverlays()
+        {
+            GenDraw.DrawRadiusRing(base.Position, this.MAX_DISTANCE);
+        }
+
+        //Saving game
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref flag_charge, "flag_charge");
+            Scribe_Values.Look(ref ReservePowerCurrent, "ReservePowerCurrent");
+
+        }
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            //Add the stock Gizmoes
+            foreach (var g in base.GetGizmos())
+            {
+                yield return g;
+            }
+
+            if (true)
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.upgradePawns();
+                act.icon = UI_UPGRADE;
+                act.defaultLabel = "Upgrade Pawn";
+                act.defaultDesc = "Upgrade Pawn";
+                //act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+
+            if (flag_charge)
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.SwitchCharge();
+                act.icon = UI_CHARGE_ON;
+                act.defaultLabel = "Charge Shields";
+                act.defaultDesc = "On";
+                // act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+            else
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.SwitchCharge();
+                act.icon = UI_CHARGE_OFF;
+                act.defaultLabel = "Charge Shields";
+                act.defaultDesc = "Off";
+                //act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+        }
+
+        #endregion
+
+        private void SwitchCharge()
+        {
+            flag_charge = !flag_charge;
+        }
+
+        private IEnumerable<CompNanoShield> ShieldCompsInRangeAndOfFaction()
+        {
+            IEnumerable<Pawn> _Pawns = this.Map.mapPawns.PawnsInFaction(Faction.OfPlayer).Where<Pawn>(t => t.Position.InHorDistOf(this.Position, this.MAX_DISTANCE));
+            IEnumerable<CompNanoShield> _Comps = _Pawns.Select(p => p.TryGetComp<CompNanoShield>());
+            return _Comps;
+        }
+
+        private void upgradePawns()
+        {
+            bool _AnyUpgraded = false;
+
+            foreach (CompNanoShield _ShieldComp in this.ShieldCompsInRangeAndOfFaction())
+            {
+                Log.Message("Adding");
+                if (!_ShieldComp.QuantumShieldActive)
+                {
+                    _ShieldComp.QuantumShieldActive = true;
+                    _AnyUpgraded = true;
+                }
+            }
+
+            if (!_AnyUpgraded)
+            {
+                Log.Message("No Paws found to add Quantum Shields to.");
+            }
+
+            return;
+        }
+
+        public int rechargePawns(int chargeToRequest)
+        {
+            int _RemainingCharge = GameComponent_NanoShield.RequestCharge(chargeToRequest);
+
+            foreach (CompNanoShield _ShieldComp in this.ShieldCompsInRangeAndOfFaction())
+            {
+                if (_ShieldComp.QuantumShieldActive)
+                {
+                    _RemainingCharge -= _ShieldComp.RechargeShield(_RemainingCharge);
+                }
+            }
+
+            GameComponent_NanoShield.ReturnCharge(_RemainingCharge);
+            return _RemainingCharge;
+        }
+
+        public override string GetInspectString()
+        {
+
+            return "Reserve = " + this.ReservePowerCurrent + " / " + Mod_EDExcalibur.Settings.BuildingReservePowerMax + " - " + GameComponent_NanoShield.GetInspectStringStatus() + Environment.NewLine + base.GetInspectString();
+        }
+
+    }
+}
---
+++ Source/ED-Excalibur/NanoShields/CompNanoShield.cs
@@ -0,0 +1,208 @@
+﻿using EnhancedDevelopment.Excalibur.Settings;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace EnhancedDevelopment.Excalibur.NanoShields
+{
+    [StaticConstructorOnStartup]
+    class CompNanoShield : ThingComp
+    {
+
+        #region Variables
+        //--Saved
+        public bool QuantumShieldActive = false;
+        public int QuantumShieldChargeLevelCurrent = 200;
+
+        //--Not Saved
+        private int KeepDisplayingTicks = 1000;
+        private int lastKeepDisplayTick = -9999;
+        private Vector3 impactAngleVect;
+
+        private static Material BubbleMat = MaterialPool.MatFrom("Other/ShieldBubble", ShaderDatabase.Transparent);
+        #endregion
+
+        #region Overrides
+
+        public override string GetDescriptionPart()
+        {
+            if (this.QuantumShieldActive)
+            {
+                return "Quantum Shield:" + QuantumShieldChargeLevelCurrent + " / " + Mod_EDExcalibur.Settings.ShieldChargeLevelMax +
+                        Environment.NewLine + base.GetDescriptionPart();
+            }
+            return base.GetDescriptionPart();
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            using (IEnumerator<Gizmo> enumerator = base.CompGetGizmosExtra().GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                {
+                    Gizmo c = enumerator.Current;
+                    yield return c;
+                    ;
+                }
+            }
+
+            if (this.QuantumShieldActive)
+            {
+                Gizmo_NanoShieldStatus opt1 = new Gizmo_NanoShieldStatus(this);
+                yield return opt1;
+            }
+        }
+
+        public override void PostPreApplyDamage(DamageInfo dinfo, out bool absorbed)
+        {
+            base.PostPreApplyDamage(dinfo, out absorbed);
+
+            //If the shield is not Active or it was alreadt absorbed or it is out of charge, return
+            if (!QuantumShieldActive || absorbed || this.QuantumShieldChargeLevelCurrent <= 0)
+            {
+                return;
+            }
+
+            //TODO Filter on Damage Type
+
+            this.QuantumShieldChargeLevelCurrent -= (int)dinfo.Amount;
+
+            absorbed = true;
+
+
+            this.lastKeepDisplayTick = Find.TickManager.TicksGame;
+
+            SoundDefOf.EnergyShield_AbsorbDamage.PlayOneShot(new TargetInfo(this.parent.Position, base.parent.Map, false));
+            this.impactAngleVect = Vector3Utility.HorizontalVectorFromAngle(dinfo.Angle);
+            Vector3 loc = base.parent.TrueCenter() + this.impactAngleVect.RotatedBy(180f) * 0.5f;
+            float num = Mathf.Min(10f, 2f + (float)dinfo.Amount / 10f);
+            MoteMaker.MakeStaticMote(loc, base.parent.Map, ThingDefOf.Mote_ExplosionFlash, num);
+            int num2 = (int)num;
+            for (int i = 0; i < num2; i++)
+            {
+                MoteMaker.ThrowDustPuff(loc, base.parent.Map, Rand.Range(0.8f, 1.2f));
+            }
+        }
+
+        //public override void CompTick()
+        //{
+        //    //Log.Message("CompTick");
+        //    base.CompTick();
+        //}
+
+        public override void PostDraw()
+        {
+            base.PostDraw();
+
+            if (this.QuantumShieldActive && this.ShouldDisplay)
+            {
+                float num1 = Mathf.Lerp(1.2f, 1.55f, 100f);
+                Vector3 drawPos = this.parent.DrawPos;
+                drawPos.y = Altitudes.AltitudeFor(AltitudeLayer.Blueprint);
+                int num2 = 7;
+                if (num2 < 8)
+                {
+                    float num3 = (float)((double)(8 - num2) / 8.0 * 0.0500000007450581);
+                    num1 -= num3;
+                }
+                float angle = (float)Rand.Range(0, 360);
+                Vector3 s = new Vector3(num1, 1f, num1);
+                Matrix4x4 matrix = new Matrix4x4();
+                matrix.SetTRS(drawPos, Quaternion.AngleAxis(angle, Vector3.up), s);
+                Graphics.DrawMesh(MeshPool.plane10, matrix, CompNanoShield.BubbleMat, 0);
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref QuantumShieldActive, "QuantumShieldActive");
+            Scribe_Values.Look(ref QuantumShieldChargeLevelCurrent, "QuantumShieldChargeLevelCurrent");
+        }
+        #endregion
+
+        public CompProperties_NanoShield Props
+        {
+            get
+            {
+                return (CompProperties_NanoShield)this.props;
+            }
+        }
+
+        private bool ShouldDisplay
+        {
+            get
+            {
+                if (!QuantumShieldActive)
+                {
+                    return false;
+                }
+
+                Pawn wearer = this.parent as Pawn;
+
+                if (wearer != null && wearer.Spawned && !wearer.Dead && !wearer.Downed)
+                {
+                    if (this.QuantumShieldChargeLevelCurrent <= 0)
+                    {
+                        return false;
+                    }
+                    if (wearer.InAggroMentalState)
+                    {
+                        return true;
+                    }
+                    if (wearer.Drafted)
+                    {
+                        return true;
+                    }
+                    if (wearer.Faction.HostileTo(Faction.OfPlayer) && !wearer.IsPrisoner)
+                    {
+                        return true;
+                    }
+                    if (Find.TickManager.TicksGame < this.lastKeepDisplayTick + this.KeepDisplayingTicks)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }
+
+        public int RechargeShield(int chargeAvalable)
+        {
+            if (!this.QuantumShieldActive || this.QuantumShieldChargeLevelCurrent >= Mod_EDExcalibur.Settings.ShieldChargeLevelMax)
+            {
+                return 0;
+            }
+
+            this.QuantumShieldChargeLevelCurrent += chargeAvalable;
+
+            if (this.QuantumShieldChargeLevelCurrent <= Mod_EDExcalibur.Settings.ShieldChargeLevelMax)
+            {
+                return chargeAvalable;
+            }
+            else
+            {
+                int _Overcharge = this.QuantumShieldChargeLevelCurrent - Mod_EDExcalibur.Settings.ShieldChargeLevelMax;
+                this.QuantumShieldChargeLevelCurrent -= _Overcharge;
+                return chargeAvalable - _Overcharge;
+            }
+        }
+
+    }
+
+    class CompProperties_NanoShield : CompProperties
+    {
+        public CompProperties_NanoShield()
+        {
+            this.compClass = typeof(CompNanoShield);
+        }
+
+    }
+
+}
