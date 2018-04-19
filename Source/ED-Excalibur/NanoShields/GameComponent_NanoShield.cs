461b6953c5512de0cde40424c9bab32fef3b7d01
Joshua W <jaxxa90@gmail.com>
2018-04-19 18:26:00 +1000

    Dialog Test

--- Source/ED-Excalibur/NanoShields/GameComponent_NanoShield.cs
+++ Source/ED-Excalibur/NanoShields/GameComponent_NanoShield.cs
@@ -24,9 +24,52 @@ namespace EnhancedDevelopment.Excalibur.NanoShields
             // GameComponent_QuantumShield.GameComp = this;
         }
 
+        //public override void GameComponentUpdate()
+        //{
+        //    Log.Message("GC.Update");
+        //    base.GameComponentUpdate();
+        //}
+
+        private string introText = "You detect a strange transmission on the Comms Console, for the past while you thought you could find a ghost of something in the static, but this time it is clear enough to make out." + Environment.NewLine + 
+        "It appears to be Destress Call directed to a specific group and asking them to make contact using an encrypted system on nonstandard frequencies.One of your Researchers thinks that with a bit of work the encryption could be broken and they could come with a device to make contact." + Environment.NewLine +
+        "Unlocked Research: Analyse Strange Signal" + Environment.NewLine +
+        "-Needs Multi-Analyzer and High Tech Bench";
+
+        private bool sent = false;
+
         public override void GameComponentTick()
         {
             base.GameComponentTick();
+
+            Log.Message("GC.Tick");
+
+            if (!sent)
+            {
+                sent = true;
+
+                DiaNode diaNode = new DiaNode(introText);
+
+                DiaOption diaOption = new DiaOption("New Test Option");
+                diaOption.action = delegate { Log.Message("Clicked"); };
+                diaOption.resolveTree = true;
+                diaNode.options.Add(diaOption);
+
+                DiaOption diaOption2 = new DiaOption("New Test Option 2");
+                diaOption2.action = delegate { Log.Message("Clicked 2"); };
+                diaOption2.resolveTree = true;
+                diaNode.options.Add(diaOption2);
+
+                DiaOption diaOption3 = new DiaOption("New Test Option 3");
+                diaOption3.action = delegate { Log.Message("Clicked 3"); };
+                diaOption3.resolveTree = true;
+                diaNode.options.Add(diaOption3);
+
+
+                Find.WindowStack.Add(new Dialog_NodeTree(diaNode,false,false,"Strange Signal Detected"));
+
+            }
+
+
 
             //Only Run every 20 Ticks.
             int currentTick = Find.TickManager.TicksGame;
