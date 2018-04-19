1e87174eb2e5a64db005678ee0f618111ac03618
Joshua W <jaxxa90@gmail.com>
2018-04-20 07:25:03 +1000

    Initial Test Transponder

---
+++ Source/ED-Excalibur/Excalibur/Building_EDSNTransponder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace EnhancedDevelopment.Excalibur.Excalibur
+{
+    class Building_EDSNTransponder : Building
+    {
+
+        public override IEnumerable<Gizmo> GetGizmos()
+        {
+            //Add the stock Gizmoes
+            foreach (var g in base.GetGizmos())
+            {
+                yield return g;
+            }
+            
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.ContactExcalibur();
+                //act.icon = UI_DIRECT_ON;
+                act.defaultLabel = "Contact Excalibur";
+                act.defaultDesc = "Contact Excalibur";
+                act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+        }
+
+        public void ContactExcalibur()
+        {
+            Log.Message("Contacting Excalibur");
+        }
+
+    }
+
+}
