51ca936d205063f586e4a09f48a73e4be2a8bd25
Joshua W <jaxxa90@gmail.com>
2018-04-20 07:52:28 +1000

    Setting initial Size

--- Source/ED-Excalibur/Excalibur/Dialog_Excalibur.cs
+++ Source/ED-Excalibur/Excalibur/Dialog_Excalibur.cs
@@ -10,11 +10,26 @@ namespace EnhancedDevelopment.Excalibur.Excalibur
 {
     class Dialog_Excalibur : Window
     {
+        public Dialog_Excalibur()
+        {
+            this.resizeable = true;
+            //this.SetInitialSizeAndPosition();
+        }
+
+
         public override void DoWindowContents(Rect inRect)
         {
 
             Widgets.ButtonText(inRect, "Button1", true, false, true);
            // throw new NotImplementedException();
+        }
+
+        public override Vector2 InitialSize
+        {
+            get
+            {
+                return new Vector2(1024f, (float)UI.screenHeight);
+            }
         }
     }
 }
