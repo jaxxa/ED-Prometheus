e0168a6c85ae0508dbce467af324c6bbda9831e0
Joshua W <jaxxa90@gmail.com>
2018-04-30 07:19:45 +1000

    Removing older Dialog Style

--- Source/ED-Excalibur/Excalibur/GameComponent_Excalibur.cs
+++ Source/ED-Excalibur/Excalibur/GameComponent_Excalibur.cs
@@ -11,6 +11,13 @@ namespace EnhancedDevelopment.Excalibur
     {
 
         #region Variables
+
+
+        private static int m_QuestStatus = 0;
+
+
+
+        //--Nano Shields
         //--Saved
         public static int ChargeLevelCurrent = 200;
 
@@ -31,56 +38,15 @@ namespace EnhancedDevelopment.Excalibur
         //    base.GameComponentUpdate();
         //}
 
-        private string introText = "You detect a strange transmission on the Comms Console, for the past while you thought you could find a ghost of something in the static, but this time it is clear enough to make out." + Environment.NewLine +
-        "It appears to be Destress Call directed to a specific group and asking them to make contact using an encrypted system on nonstandard frequencies.One of your Researchers thinks that with a bit of work the encryption could be broken and they could come with a device to make contact." + Environment.NewLine +
-        "Unlocked Research: Analyse Strange Signal" + Environment.NewLine +
-        "-Needs Multi-Analyzer and High Tech Bench";
-        
-        private bool _ShownInitial = true;
-
         public override void GameComponentTick()
         {
             base.GameComponentTick();
 
             Log.Message("GC.Tick");
 
-            if (!_ShownInitial)
-            {
-                _ShownInitial = true;
-
-                DiaNode diaNode = new DiaNode(introText);
-
-                DiaOption diaOption = new DiaOption("Ok");
-                diaOption.action = delegate
-                {
-                    Log.Message("Clicked");
-
-                    //Find.ResearchManager
-                    ResearchProjectDef _Quest = DefDatabase<ResearchProjectDef>.GetNamed("ED-Excalibur_Quest_Unlock");
-                    _Quest.prerequisites.Remove(_Quest);
-                    //_Quest.prerequisites.Clear();
-                };
-                diaOption.resolveTree = true;
-                diaNode.options.Add(diaOption);
-
-                DiaOption diaOption2 = new DiaOption("New Test Option 2");
-                diaOption2.action = delegate { Log.Message("Clicked 2"); };
-                diaOption2.resolveTree = true;
-                diaNode.options.Add(diaOption2);
-
-                //DiaOption diaOption3 = new DiaOption("New Test Option 3");
-                //diaOption3.action = delegate { Log.Message("Clicked 3"); };
-                //diaOption3.resolveTree = true;
-                //diaNode.options.Add(diaOption3);
-
-                Find.WindowStack.Add(new Dialog_NodeTree(diaNode, false, false, "Strange Signal Detected"));
-
-            }
             this.NanoShieldTick();
         }
-
-        private static int m_QuestStatus = 1;
-
+        
         public static void ContactExcalibur()
         {
 
@@ -125,8 +91,7 @@ namespace EnhancedDevelopment.Excalibur
 
 
         }
-
-
+        
         #region Nano Shields
 
         private void NanoShieldTick()
@@ -183,6 +148,8 @@ namespace EnhancedDevelopment.Excalibur
 
 
         #endregion //Nano Shields
+
+
 
     }
 }
