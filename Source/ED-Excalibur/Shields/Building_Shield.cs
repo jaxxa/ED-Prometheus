24773286c969092b4a098ea03ca079dc8d9211fc
Joshua W <jaxxa90@gmail.com>
2018-05-02 18:42:45 +1000

    Moving Shield Field Drawing to Comp

--- Source/ED-Excalibur/Shields/Building_Shield.cs
+++ Source/ED-Excalibur/Shields/Building_Shield.cs
@@ -494,57 +494,9 @@ namespace EnhancedDevelopment.Excalibur.Shields
         public override void Draw()
         {
             base.Draw();
-            this.DrawShields();
+           // this.DrawShields();
         }
 
-        /// <summary>
-        /// Draw the shield Field
-        /// </summary>
-        public void DrawShields()
-        {
-            if (!this.IsActive() || !this.m_ShowVisually_Active)
-            {
-                return;
-            }
-
-            //Draw field
-            this.DrawField(EnhancedDevelopment.Excalibur.Shields.Utilities.VectorsUtils.IntVecToVec(base.Position));
-
-        }
-
-        public override void DrawExtraSelectionOverlays()
-        {
-            //    GenDraw.DrawRadiusRing(base.Position, shieldField.shieldShieldRadius);
-        }
-
-        public void DrawSubField(IntVec3 center, float radius)
-        {
-            this.DrawSubField(EnhancedDevelopment.Excalibur.Shields.Utilities.VectorsUtils.IntVecToVec(center), radius);
-        }
-
-        //Draw the field on map
-        public void DrawField(Vector3 center)
-        {
-            DrawSubField(center, m_Field_Radius);
-        }
-
-        public void DrawSubField(Vector3 position, float shieldShieldRadius)
-        {
-            position = position + (new Vector3(0.5f, 0f, 0.5f));
-
-            Vector3 s = new Vector3(shieldShieldRadius, 1f, shieldShieldRadius);
-            Matrix4x4 matrix = default(Matrix4x4);
-            matrix.SetTRS(position, Quaternion.identity, s);
-
-            if (currentMatrialColour == null)
-            {
-                //Log.Message("Creating currentMatrialColour");
-                currentMatrialColour = SolidColorMaterials.NewSolidColorMaterial(new Color(m_ColourRed, m_ColourGreen, m_ColourBlue, 0.15f), ShaderDatabase.MetaOverlay);
-            }
-
-            UnityEngine.Graphics.DrawMesh(EnhancedDevelopment.Excalibur.Shields.Utilities.Graphics.CircleMesh, matrix, currentMatrialColour, 0);
-
-        }
 
         #endregion
 
