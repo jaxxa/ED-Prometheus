f934d0d1bc0cc1a147a26caf8936659f8fe7b84a
Joshua W <jaxxa90@gmail.com>
2018-06-04 18:21:26 +1000

    Transporting and recalling from the same Transporter.

--- Source/ED-Excalibur/Transporter/Comp_Transporter.cs
+++ Source/ED-Excalibur/Transporter/Comp_Transporter.cs
@@ -10,7 +10,7 @@ namespace EnhancedDevelopment.Excalibur.Transporter
 {
     class Comp_Transporter : ThingComp
     {
-        
+
         public override IEnumerable<Gizmo> CompGetGizmosExtra()
         {
             //return base.CompGetGizmosExtra();
@@ -25,10 +25,53 @@ namespace EnhancedDevelopment.Excalibur.Transporter
             {
                 Command_Action act = new Command_Action();
                 //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
-                act.action = () => this.SendMaterials();
+                act.action = () => this.ProvideMatrials();
                 //    act.icon = UI_ADD_RESOURCES;
-                act.defaultLabel = "SendMaterials";
-                act.defaultDesc = "SendMaterials";
+                act.defaultLabel = "ProvideMatrials";
+                act.defaultDesc = "ProvideMatrials";
+                act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+
+
+            if (true)
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.TransportColonists();
+                //    act.icon = UI_ADD_RESOURCES;
+                act.defaultLabel = "TransportColonists";
+                act.defaultDesc = "TransportColonists";
+                act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+
+            if (true)
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.TransportThings();
+                //    act.icon = UI_ADD_RESOURCES;
+                act.defaultLabel = "TransportThings";
+                act.defaultDesc = "TransportThings";
+                act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+
+            if (true)
+            {
+                Command_Action act = new Command_Action();
+                //act.action = () => Designator_Deconstruct.DesignateDeconstruct(this);
+                act.action = () => this.TransportRecall();
+                //    act.icon = UI_ADD_RESOURCES;
+                act.defaultLabel = "TransportRecall";
+                act.defaultDesc = "TransportRecall";
                 act.activateSound = SoundDef.Named("Click");
                 //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
                 //act.groupKey = 689736;
@@ -36,23 +79,19 @@ namespace EnhancedDevelopment.Excalibur.Transporter
             }
 
         }
-        
-        private void SendMaterials()
+
+        List<Thing> m_TransportBuffer = new List<Thing>();
+
+        private void ProvideMatrials()
         {
             List<Thing> _FoundThings = GenRadial.RadialDistinctThingsAround(this.parent.Position, this.parent.Map, 5, true).Where(x => x.def.category == ThingCategory.Item).Where(x => x.Spawned).Where(x => x.def.defName == "Steel").ToList();
 
             if (_FoundThings.Any())
             {
-                //Log.Message("Found:" + foundThings.Count().ToString());
-
-                //GameComponent_Transporter.TransportControler().StoreThing(_FoundThings);
-
                 _FoundThings.ForEach(_x =>
                 {
 
                     GameComponent_Excalibur.Instance.Comp_Quest.AddReserveMaterials(_x.stackCount);
-
-                    //Log.Message("Removing: " + x.def.defName);
                     this.DisplayTransportEffect(_x);
 
                     _x.DeSpawn();
@@ -65,7 +104,72 @@ namespace EnhancedDevelopment.Excalibur.Transporter
             }
 
         }
-        
+
+        private void TransportColonists()
+        {
+            // List<Thing> _FoundThings = GenRadial.RadialDistinctThingsAround(this.parent.Position, this.parent.Map, 5, true).Where(x => x.def.category == ThingCategory.Item).Where(x => x.Spawned).Where(x => x.def.defName == "Steel").ToList();
+
+
+            List<Pawn> _Pawns = this.parent.Map.mapPawns.PawnsInFaction(Faction.OfPlayer).Where<Pawn>(t => t.Position.InHorDistOf(this.parent.Position, 5)).ToList();
+
+            if (_Pawns.Any())
+            {
+                _Pawns.ForEach(_x =>
+                {
+
+                    this.m_TransportBuffer.Add(_x);
+                    //GameComponent_Excalibur.Instance.Comp_Quest.AddReserveMaterials(_x.stackCount);
+                    this.DisplayTransportEffect(_x);
+
+                    _x.DeSpawn();
+
+                    // Tell the MapDrawer that here is something thats changed
+                    this.parent.Map.mapDrawer.MapMeshDirty(_x.Position, MapMeshFlag.Things, true, false);
+                });
+
+
+            }
+        }
+
+        private void TransportThings()
+        {
+
+            List<Thing> _FoundThings = GenRadial.RadialDistinctThingsAround(this.parent.Position, this.parent.Map, 5, true).Where(x => x.def.category == ThingCategory.Item).Where(x => x.Spawned).ToList();
+
+            if (_FoundThings.Any())
+            {
+                _FoundThings.ForEach(_x =>
+                {
+
+
+                    this.m_TransportBuffer.Add(_x);
+
+                    //GameComponent_Excalibur.Instance.Comp_Quest.AddReserveMaterials(_x.stackCount);
+                    this.DisplayTransportEffect(_x);
+
+                    _x.DeSpawn();
+
+                    // Tell the MapDrawer that here is something thats changed
+                    this.parent.Map.mapDrawer.MapMeshDirty(_x.Position, MapMeshFlag.Things, true, false);
+                });
+
+            }
+            
+        }
+
+        private void TransportRecall()
+        {
+
+            this.m_TransportBuffer.ForEach(_X =>
+            {
+                GenPlace.TryPlaceThing(_X, this.parent.Position, this.parent.Map, ThingPlaceMode.Near);
+                this.DisplayTransportEffect(_X);
+            }
+            );
+
+            this.m_TransportBuffer.Clear();
+        }
+
         private void DisplayTransportEffect(Thing thingToTransport)
         {
             MoteMaker.MakeStaticMote(thingToTransport.Position, thingToTransport.Map, ThingDefOf.Mote_ExplosionFlash, 10);
