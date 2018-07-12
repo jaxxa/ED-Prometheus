146f187de572d891ce70d253f8a018adaf2b833d
Joshua W <jaxxa90@gmail.com>
2018-07-12 18:34:55 +1000

    Adding Initial Work on Ship Systems

---
+++ Source/ED-Excalibur/Quest/ShipSystems/ShipSystem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnhancedDevelopment.Excalibur.Quest
+{
+    class ShipSystem
+    {
+    }
+}
---
+++ Source/ED-Excalibur/Quest/ShipSystems/ShipSystem_Hull.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnhancedDevelopment.Excalibur.Quest.ShipSystems
+{
+    class ShipSystem_Hull : ShipSystem
+    {
+    }
+}
