bac19bf2bef09b84bd1e04c581433e24b6b695bd
Joshua W <jaxxa90@gmail.com>
2018-08-16 07:54:19 +1000

    Shield research unlock by system

--- Source/ED-Excalibur/Quest/ShipSystems/ShipSystem_Shield.cs
+++ Source/ED-Excalibur/Quest/ShipSystems/ShipSystem_Shield.cs
@@ -20,7 +20,18 @@ namespace EnhancedDevelopment.Excalibur.Quest.ShipSystems
 
         public override void ApplyRequiredResearchUnlocks()
         {
-           // ResearchHelper.QuestComplete("Research_ED_OmniGel");
+            if (this.CurrentLevel >= 1)
+            {
+                ResearchHelper.QuestComplete("Research_ED_ShieldGenerators");
+            }
+            if (this.CurrentLevel >= 2)
+            {
+                ResearchHelper.QuestComplete("Research_ED_ShieldUpgrades");
+            }
+            if (this.CurrentLevel >= 3)
+            {
+                ResearchHelper.QuestComplete("Research_ED_Personal_Quantum_Shield");
+            }
         }
     }
 }
