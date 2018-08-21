ea1114330e8c9994a1d012cf4c073d848b244398
Joshua W <jaxxa90@gmail.com>
2018-08-22 08:08:11 +1000

    Setting up Tabbing

--- Source/ED-Excalibur/Quest/Dialog/Dialog_Excalibur.cs
+++ Source/ED-Excalibur/Quest/Dialog/Dialog_Excalibur.cs
@@ -10,6 +10,16 @@ namespace EnhancedDevelopment.Excalibur.Quest.Dialog
 {
     class Dialog_Excalibur : Window
     {
+
+        public enum EnumDialogTabSelection
+        {
+            SystemStatus,
+            Buildings
+
+        }
+
+        private EnumDialogTabSelection m_CurrentTab = EnumDialogTabSelection.Buildings;
+
         public Dialog_Excalibur()
         {
             this.resizeable = false;
@@ -20,10 +30,10 @@ namespace EnhancedDevelopment.Excalibur.Quest.Dialog
             this.doCloseX = false;
             this.closeOnClickedOutside = false;
             this.doCloseX = true;
-            
+
 
             //this.SetInitialSizeAndPosition();
-        } 
+        }
 
         public override void DoWindowContents(Rect inRect)
         {
@@ -46,12 +56,14 @@ namespace EnhancedDevelopment.Excalibur.Quest.Dialog
 
         Vector2 m_ScrollPosition = new Vector2();
 
-       // private float m_ViewHeight = 99999f;
+        // private float m_ViewHeight = 99999f;
+
+
+        int _TabSelectionHeight = 30;
+        int _FooterSectionHeight = 35;
 
         private void InitialWindowContents(Rect totalCanvas)
         {
-            int _TabSelectionHeight = 30;
-            int _FooterSectionHeight = 35;
             Rect _TabSelectionRect = totalCanvas.TopPartPixels(_TabSelectionHeight);
 
 
@@ -62,50 +74,27 @@ namespace EnhancedDevelopment.Excalibur.Quest.Dialog
             WidgetRow _ButtonWidgetRow = new WidgetRow(_TabSelectionRect.x, _TabSelectionRect.y, UIDirection.RightThenDown, 99999f, 4f);
             if (_ButtonWidgetRow.ButtonText("Buildings", null, true, true))
             {
+                this.m_CurrentTab = EnumDialogTabSelection.Buildings;
                 //Find.WindowStack.Add(new Dialog_BillConfig(this, ((Thing)base.billStack.billGiver).Position));
             }
 
             if (_ButtonWidgetRow.ButtonText("System Status", null, true, true))
             {
+                this.m_CurrentTab = EnumDialogTabSelection.SystemStatus;
                 //Find.WindowStack.Add(new Dialog_BillConfig(this, ((Thing)base.billStack.billGiver).Position));
             }
             Widgets.DrawLineHorizontal(_TabSelectionRect.xMin, _TabSelectionRect.yMax, _TabSelectionRect.width);
 
             // Main Content ---------------------------------------------------
-            
+
             Rect _WindowContent = new Rect(totalCanvas.xMin, _TabSelectionRect.yMax, totalCanvas.width, totalCanvas.height - _TabSelectionHeight - _FooterSectionHeight);
             //Widgets.ButtonText(_WindowContent, "_WindowContent", true, false, true);
 
 
-            float _ViewContentHeight = (Core.GameComponent_Excalibur.Instance.Comp_Quest.m_ShipSystems.Count() + 1) * ShipSystem.m_Height + 6f;
-
-            Widgets.TextArea(_WindowContent.TopPartPixels(20), "Ship Status", true);
-
-            GUI.color = Color.white;
-            Rect outRect = new Rect(_WindowContent.x, _WindowContent.y + 20, _WindowContent.width, _WindowContent.height - _FooterSectionHeight);
-            Rect viewRect = new Rect(0f, 0f, outRect.width - 16f, _ViewContentHeight);
-
-            //Above scroll view
-            Widgets.DrawLineHorizontal(outRect.xMin, outRect.yMin , outRect.width);
-
-            Widgets.BeginScrollView(outRect, ref m_ScrollPosition, viewRect, true);
-
-            float num = 0f;
-            for (int i = 0; i < Core.GameComponent_Excalibur.Instance.Comp_Quest.m_ShipSystems.Count(); i++)
+            if (this.m_CurrentTab == EnumDialogTabSelection.SystemStatus)
             {
-                ShipSystem _BuildingInProgress = Core.GameComponent_Excalibur.Instance.Comp_Quest.m_ShipSystems[i];
-                Rect rect3 = _BuildingInProgress.DoInterface(0f, num, viewRect.width, i);
-                //if (!bill.DeletedOrDereferenced && Mouse.IsOver(rect3))
-                //{
-                //    result = bill;
-                //}
-                num += rect3.height + 6f;
-                Widgets.DrawLineHorizontal(viewRect.x, num, viewRect.width);
+                this.DoSystemStatusGui(_WindowContent);
             }
-
-
-            Widgets.EndScrollView();
-
 
             // Footer (System Status) -----------------------------------------
 
@@ -128,6 +117,39 @@ namespace EnhancedDevelopment.Excalibur.Quest.Dialog
             _listing_Standard_ShieldChargeLevelMax.End();
 
 
+        }
+
+
+        public void DoSystemStatusGui(Rect windowContent)
+        {
+
+            float _ViewContentHeight = (Core.GameComponent_Excalibur.Instance.Comp_Quest.m_ShipSystems.Count() + 1) * ShipSystem.m_Height + 6f;
+
+            Widgets.TextArea(windowContent.TopPartPixels(20), "Ship Status", true);
+
+            GUI.color = Color.white;
+            Rect outRect = new Rect(windowContent.x, windowContent.y + 20, windowContent.width, windowContent.height - _FooterSectionHeight);
+            Rect viewRect = new Rect(0f, 0f, outRect.width - 16f, _ViewContentHeight);
+
+            //Above scroll view
+            Widgets.DrawLineHorizontal(outRect.xMin, outRect.yMin, outRect.width);
+
+            Widgets.BeginScrollView(outRect, ref m_ScrollPosition, viewRect, true);
+
+            float num = 0f;
+            for (int i = 0; i < Core.GameComponent_Excalibur.Instance.Comp_Quest.m_ShipSystems.Count(); i++)
+            {
+                ShipSystem _BuildingInProgress = Core.GameComponent_Excalibur.Instance.Comp_Quest.m_ShipSystems[i];
+                Rect rect3 = _BuildingInProgress.DoInterface(0f, num, viewRect.width, i);
+                //if (!bill.DeletedOrDereferenced && Mouse.IsOver(rect3))
+                //{
+                //    result = bill;
+                //}
+                num += rect3.height + 6f;
+                Widgets.DrawLineHorizontal(viewRect.x, num, viewRect.width);
+            }
+
+            Widgets.EndScrollView();
         }
     }
 }
