dba2fae4a6de737e3b43f326476b5684e6bc0485
Joshua W <jaxxa90@gmail.com>
2018-09-07 08:01:08 +1000

    Initial Setup for MolecularCompressor

---
+++ Source/ED-Excalibur/Fabrication/Comp_MolecularCompressor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace EnhancedDevelopment.Excalibur.Fabrication
+{
+    class Comp_MolecularCompressor : ThingComp
+    {
+
+        public CompProperties_MolecularCompressor Properties;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+
+            this.Properties = ((CompProperties_MolecularCompressor)this.props);
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            Log.Message("Tick");
+        }
+
+    }
+}
