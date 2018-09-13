f56d7c811b4270f3d17fd69b25ab0bd73c1c772a
Joshua W <jaxxa90@gmail.com>
2018-09-13 17:51:43 +1000

    Adding the ability to specify the DropPod location of deployable buildings.

--- Source/ED-Excalibur/Quest/ITab_Fabrication.cs
+++ Source/ED-Excalibur/Quest/ITab_Fabrication.cs
@@ -14,13 +14,11 @@ namespace EnhancedDevelopment.Excalibur.Quest
     class ITab_Fabrication : ITab
     {
 
+
+        public TargetingParameters targetingParams = new TargetingParameters();
+
         //private Comp_ShieldGenerator _CachedComp;
 
-        //public ITab_ShieldGenerator() : base()
-        //{
-        //    _CachedComp = 
-
-        //}
 
         private static readonly Vector2 WinSize = new Vector2(420f, 480f);
 
@@ -50,30 +48,52 @@ namespace EnhancedDevelopment.Excalibur.Quest
             }
         }
 
-        public ITab_Fabrication()
+        public ITab_Fabrication() : base()
         {
             base.size = ITab_Fabrication.WinSize;
             base.labelKey = "Fabrication";
             //base.tutorTag = "ShieldGenenerator";
+
+            targetingParams.canTargetLocations = true;
         }
+
 
         protected override void FillTab()
         {
+            int _TargetingBarHeight = 30;
 
             Vector2 _WindowSize = ITab_Fabrication.WinSize;
             Rect _DrawingSpace = new Rect(0f, 0f, _WindowSize.x, _WindowSize.y).ContractedBy(10f);
 
-            Rect _MainWindow = _DrawingSpace.TopPartPixels(_DrawingSpace.height - 25);
-            Rect _InfoBar = _DrawingSpace.BottomPartPixels(25);
+            Rect _MainWindow = _DrawingSpace.TopPartPixels(_DrawingSpace.height - _TargetingBarHeight);
+            Rect _TargetingBar = _DrawingSpace.BottomPartPixels(_TargetingBarHeight);
+            
+            if (Widgets.ButtonText(_TargetingBar.LeftHalf(), "Targeting", true, true, true))
+            {
+                
+                Find.Targeter.BeginTargeting(this.targetingParams, delegate (LocalTargetInfo target)
+                {
+                    this.SelectedCompTransponder.DropLocation = target.Cell;
+                }, null, null, null);
+            }
+
+            Widgets.Label(_TargetingBar.RightHalf(), this.SelectedCompTransponder.DropLocation.x + " " + this.SelectedCompTransponder.DropLocation.z);
+
+            IntVec3 _DropLocation = this.SelectedCompTransponder.parent.Position;
+
+            if (!IntVec3.Equals(this.SelectedCompTransponder.DropLocation, IntVec3.Invalid))
+            {
+                _DropLocation = this.SelectedCompTransponder.DropLocation;
+            }
 
 
-            Dialog_Excalibur.DoGuiFabrication(_MainWindow, 
-                                           true,
-                                           this.SelectedCompTransponder.parent.Position, 
-                                           this.SelectedCompTransponder.parent.Map);
+            Dialog_Excalibur.DoGuiFabrication(_MainWindow,
+                                              true,
+                                              _DropLocation,
+                                              this.SelectedCompTransponder.parent.Map);
 
         }
-        
+
     } //Class
 
 }
