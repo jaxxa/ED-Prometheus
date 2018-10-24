a4d6d6d3ac4b651284165f39c5b4acff4d2e463d
Joshua W <jaxxa90@gmail.com>
2018-10-25 07:33:25 +1100

    Adding button to activate Drill

--- Source/ED-Prometheus/LaserDrill/Comp_LaserDrill.cs
+++ Source/ED-Prometheus/LaserDrill/Comp_LaserDrill.cs
@@ -4,12 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 using Verse;
 
 namespace EnhancedDevelopment.Prometheus.LaserDrill
 {
+    [StaticConstructorOnStartup]
     class Comp_LaserDrill : ThingComp
     {
+        
+        #region Variables
+
         //Saved
         private int DrillWork;
 
@@ -18,8 +23,18 @@ namespace EnhancedDevelopment.Prometheus.LaserDrill
         private CompFlickable _FlickComp;
         private CompProperties_LaserDrill Properties;
 
+        private static Texture2D UI_LASER_ACTIVATE;
 
-        public int duration = 1000;
+        #endregion Variables
+        
+        #region Initilisation
+
+
+        static Comp_LaserDrill()
+        {
+
+            UI_LASER_ACTIVATE = ContentFinder<Texture2D>.Get("UI/DirectOn", true);
+        }
 
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
@@ -34,6 +49,11 @@ namespace EnhancedDevelopment.Prometheus.LaserDrill
             }
 
         }
+
+        #endregion Initilisation
+
+        #region Overrides
+
 
         public override void PostExposeData()
         {
@@ -60,35 +80,8 @@ namespace EnhancedDevelopment.Prometheus.LaserDrill
                 this.DrillWork = this.DrillWork - 1;
             }
 
-            if (this.DrillWork <= 0)
-            {
 
-                if (this.Properties.FillMode)
-                {
-                    if (this.FindClosestGuyser() != null)
-                    {
-                        Messages.Message("SteamGeyser Removed.", MessageTypeDefOf.TaskCompletion);
-                        this.FindClosestGuyser().DeSpawn();
-                        this.TriggerLaser();
-                        this.parent.Destroy(DestroyMode.Vanish);
-                    }
-                    else
-                    {
-                        Messages.Message("SteamGeyser not found to Remove.", MessageTypeDefOf.TaskCompletion);
-                    }
-                }
-                else
-                {
-                    Messages.Message("SteamGeyser Created.", MessageTypeDefOf.TaskCompletion);
-                    this.TriggerLaser();
-                    GenSpawn.Spawn(ThingDef.Named("SteamGeyser"), this.parent.Position, this.parent.Map);
 
-                    //Destroy
-
-                    this.parent.Destroy(DestroyMode.Vanish);
-
-                }
-            }
             base.CompTickRare();
 
         }
@@ -140,6 +133,36 @@ namespace EnhancedDevelopment.Prometheus.LaserDrill
             return _StringBuilder.ToString();
         }
 
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            //return base.CompGetGizmosExtra();
+
+            //Add the stock Gizmoes
+            foreach (var g in base.CompGetGizmosExtra())
+            {
+                yield return g;
+            }
+
+            //if (this.DrillWork <= 0)
+            if (true)
+            {
+                Command_Action act = new Command_Action();
+                act.action = () => this.TriggerLaser();
+                act.icon = UI_LASER_ACTIVATE;
+                act.defaultLabel = "Block Direct";
+                act.defaultDesc = "On";
+                act.activateSound = SoundDef.Named("Click");
+                //act.hotKey = KeyBindingDefOf.DesignatorDeconstruct;
+                //act.groupKey = 689736;
+                yield return act;
+            }
+        } //CompGetGizmosExtra()
+
+        #endregion Overrides
+
+        #region Methods
+
         private void CalculateWorkStart()
         {
             if (this.Properties.FillMode)
@@ -183,10 +206,45 @@ namespace EnhancedDevelopment.Prometheus.LaserDrill
         public void TriggerLaser()
         {
 
-            IntVec3 _Position = IntVec3.FromVector3(new UnityEngine.Vector3(parent.Position.x, parent.Position.y, parent.Position.z - 2));
 
+            if (this.Properties.FillMode)
+            {
+                if (this.FindClosestGuyser() != null)
+                {
+                    Messages.Message("SteamGeyser Removed.", MessageTypeDefOf.TaskCompletion);
+                    this.FindClosestGuyser().DeSpawn();
+                    this.ShowLaserVisually();
+                    this.parent.Destroy(DestroyMode.Vanish);
+                }
+                else
+                {
+                    Messages.Message("SteamGeyser not found to Remove.", MessageTypeDefOf.TaskCompletion);
+                }
+            }
+            else
+            {
+                Messages.Message("SteamGeyser Created.", MessageTypeDefOf.TaskCompletion);
+                this.ShowLaserVisually();
+                GenSpawn.Spawn(ThingDef.Named("SteamGeyser"), this.parent.Position, this.parent.Map);
+
+                //Destroy
+
+                this.parent.Destroy(DestroyMode.Vanish);
+
+            }
+
+
+        }
+
+        private void ShowLaserVisually()
+        {
+            IntVec3 _Position = IntVec3.FromVector3(new UnityEngine.Vector3(parent.Position.x, parent.Position.y, parent.Position.z - 2));
             LaserDrillVisual _LaserDrillVisual = (LaserDrillVisual)GenSpawn.Spawn(ThingDef.Named("LaserDrillVisual"), _Position, parent.Map, WipeMode.Vanish);
         }
 
-    }
+        #endregion
+
+    } //Comp_LaserDrill
+
 }
+
