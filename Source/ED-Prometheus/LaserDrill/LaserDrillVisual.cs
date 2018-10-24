4b17207b3eccb4de34d8cbf4c484260cea63af3e
Joshua W <jaxxa90@gmail.com>
2018-10-24 18:12:19 +1100

    Adding Laser Drills that start fires.

--- Source/ED-Prometheus/LaserDrill/LaserDrillVisual.cs
+++ Source/ED-Prometheus/LaserDrill/LaserDrillVisual.cs
@@ -26,7 +26,42 @@ namespace EnhancedDevelopment.Prometheus.LaserDrill
             MoteMaker.MakeBombardmentMote(this.Position, this.Map);
             MoteMaker.MakePowerBeamMote(this.Position, this.Map);
         }
-        
+
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (!base.Destroyed)
+            {
+                if (Find.TickManager.TicksGame % 20 == 0)
+                {
+                    this.StartRandomFire();
+                }
+            }
+        }
+
+        private static readonly SimpleCurve DistanceChanceFactor = new SimpleCurve
+        {
+            {
+                new CurvePoint(0f, 1f),
+                true
+            },
+            {
+                new CurvePoint(10f, 0.1f),
+                true
+            }
+        };
+
+        private void StartRandomFire()
+        {
+            IntVec3 c = (from x in GenRadial.RadialCellsAround(base.Position, 25f, true)
+                         where x.InBounds(base.Map)
+                         select x).RandomElementByWeight((IntVec3 x) => LaserDrillVisual.DistanceChanceFactor.Evaluate(x.DistanceTo(base.Position)));
+            FireUtility.TryStartFireIn(c, base.Map, Rand.Range(0.1f, 0.925f));
+        }
+
+
+
         public override void Draw()
         {
             base.Comps_PostDraw();
