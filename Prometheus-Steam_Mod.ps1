fb81696f5032bf492dcb056dd01157d179d17f2d
Jaxxa <jaxxa90@gmail.com>
2020-02-17 18:23:59 +1100

    Updating About.xml and Launch Script

---
+++ Prometheus-Steam_Mod.ps1
@@ -0,0 +1,10 @@
+﻿$RimworldPath = "C:\Games\Steam\steamapps\common\RimWorld"
+
+$ExePath = $RimworldPath + "\RimWorldWin64.exe"
+$ModDestination = $RimworldPath + "\Mods\ED-Prometheus"
+
+Remove-Item -Path $ModDestination -Recurse
+
+Copy-Item -Path "C:\~Git\Jaxxa-Rimworld\~SubModules\ED-Prometheus\ED-Prometheus" -Destination $ModDestination -Recurse
+
+start -FilePath $ExePath -ArgumentList "-savedatafolder=C:\~Git\RimworldSaves_1.1\ModTest"
