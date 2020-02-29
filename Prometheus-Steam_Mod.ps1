a9064107061309a85f23a6a299ba3fc36ff5a569
Jaxxa <jaxxa90@gmail.com>
2020-02-29 11:52:57 +1100

    Updating start script

--- Prometheus-Steam_Mod.ps1
+++ Prometheus-Steam_Mod.ps1
@@ -1,10 +1,12 @@
-﻿$RimworldPath = "C:\Games\Steam\steamapps\common\RimWorld"
+﻿$ModName = "ED-Prometheus"
+$RimworldPath = "C:\Games\Steam\steamapps\common\RimWorld"
 
 $ExePath = $RimworldPath + "\RimWorldWin64.exe"
-$ModDestination = $RimworldPath + "\Mods\ED-Prometheus"
+$ModSource = "C:\~Git\Jaxxa-Rimworld\~SubModules\" + $ModName +"\" + $ModName
+$ModDestination = $RimworldPath + "\Mods\" + $ModName
 
 Remove-Item -Path $ModDestination -Recurse
 
-Copy-Item -Path "C:\~Git\Jaxxa-Rimworld\~SubModules\ED-Prometheus\ED-Prometheus" -Destination $ModDestination -Recurse
+Copy-Item -Path $ModSource -Destination $ModDestination -Recurse
 
 start -FilePath $ExePath -ArgumentList "-savedatafolder=C:\~Git\RimworldSaves_1.1\ModTest"
